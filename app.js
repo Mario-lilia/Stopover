54f1d0c1aca0e044ec157a27ca76debaa0713788
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-02-06 16:15:14 +0100

    feat: Create Layout, fixed #8

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const expressLayouts = require('express-ejs-layouts');
 
 const auth = require('./routes/auth.routes')
 const index = require('./routes/index.routes');
@@ -15,6 +15,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout/main');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
