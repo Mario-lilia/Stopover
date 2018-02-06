ee54a8361e210072a6d04fba0b3a75a20c2dd326
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-02-06 17:26:48 +0100

    aprendiendo

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -5,3 +5,9 @@ module.exports.login = ((req, res, next) => {
   res.render('auth/login')
 
 })
+module.exports.loginPost = ((req, res, next) => {
+ let nombre = req.body.name;
+  console.log(nombre);
+   res.render('auth/login',{nombre:nombre});
+
+})
