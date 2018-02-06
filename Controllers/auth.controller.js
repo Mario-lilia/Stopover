4cf87ee677e7c6777145284effb06da63a8198db
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-02-06 15:33:03 +0100

    feat: Create login, fixed #9

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,3 +1,7 @@
 module.exports.signup = ((req, res, next) => {
     res.render('auth/signup')
 })
+module.exports.login = ((req, res, next) => {
+  res.render('auth/login')
+
+})
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller')
 router.get('/signup', authController.signup);
 //router.post('/signup', authController.doSignup);
-
+router.get('/login',authController.login);
 
 module.exports = router;
