306b878276fc7df56c6e5954439e30aebf6422c9
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-02-15 17:43:16 +0100

    feat: create folder configs, fixed #23

--- models/users.models.js
+++ models/users.models.js
@@ -42,5 +42,6 @@ userSchema.methods.checkPassword = function (password) {
     return bcrypt.compare(password, this.password);
 }
 
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
